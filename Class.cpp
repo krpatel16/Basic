0a94b49a4c2926946d65ba27aefe9dc0972f9e54
kaushal <kaushalpatel16@gmail.com>
2019-07-19 21:47:30 +0200

    find the requied volume with set and get

---
+++ Class.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class box
+{
+	int h, w, d, vol;
+
+    public:
+	box()
+	{
+	  cout << " Enter Height = " ;
+	  cin >> h;
+	  cout << " Enter width = " ;
+	  cin >> w;
+	  cout << " Enter depth = " ;
+	  cin >> d;
+	}
+
+	void set_volume(int siz)
+	{
+	  vol = siz;
+	}
+
+    int get_volume()
+    {
+        cout << "reqire volume is = " << vol;
+        cout << endl;
+        return vol;
+    }
+
+	~box()
+	{
+	    cout << "destroy the block" << endl;
+	}
+	int volume()
+	{
+		int size;
+		size = h * w * d;
+		cout << "Calculted Volume = ";
+        cout << size << endl;
+		return size;
+	}
+
+};
+int main()
+{
+    int size;
+    box b;
+    //b.volume();
+    size = b.volume();
+
+
+    if (size == 60)
+    {
+      cout << "requied volume" << endl;
+    }else
+    {
+      b.set_volume(60);
+      b.get_volume();
+     // b.volume();
+    }
+
+    //cin.get(;)
+
+    return 0;
+}
